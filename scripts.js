3bd78efdd0b4f939c3f317b58974d432a556092e
cdrishinski <cdrishinski@gmail.com>
2019-10-11 21:13:12 -0600

    adding listener to screen and play button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 // Get our elements
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress-filled')
-const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelector('[data-skip]')
-const ranges = player.querySelector('.player__slider')
-const video = player.querySelector('.viewer')
-const video = player.querySelector('.viewer')
-const video = player.querySelector('.viewer')
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress-filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelector("[data-skip]");
+const ranges = player.querySelector(".player__slider");
 
 //build functions
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
 //hook up event listeners
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
