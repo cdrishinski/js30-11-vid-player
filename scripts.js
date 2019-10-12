1a4785d76d33789bc084780690da93817a0f4a2a
cdrishinski <cdrishinski@gmail.com>
2019-10-11 21:32:58 -0600

    adding time skip functionality

--- scripts.js
+++ scripts.js
@@ -4,20 +4,31 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress-filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 //build functions
 function togglePlay() {
-  const method = video.paused ? "play" : "pause";
-  video[method]();
+    const method = video.paused ? "play" : "pause";
+    video[method]();
 }
 
-function updateButton(){
-    this.pause ? '►' : '❚ ❚';
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
 }
+
+function skip() {
+    console.log(this.dataset.skip)
+    console.log(video.currentTime)
+    video.currentTime += parseFloat(this.dataset.skip)
+    
+}
+
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip))
