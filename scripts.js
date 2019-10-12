bc0e1f437ecb1a8ec65b54463e0aeb2b78cc00c4
cdrishinski <cdrishinski@gmail.com>
2019-10-11 20:47:25 -0600

    adding query selectors

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress-filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelector('[data-skip]')
+const ranges = player.querySelector('.player__slider')
+const video = player.querySelector('.viewer')
+const video = player.querySelector('.viewer')
+const video = player.querySelector('.viewer')
+
+//build functions
+//hook up event listeners
