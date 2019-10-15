e3cc6cbccd68275b95b4dbaac275f812c50ea304
cdrishinski <cdrishinski@gmail.com>
2019-10-14 18:33:12 -0600

    adding progress bar functionality

--- scripts.js
+++ scripts.js
@@ -29,21 +29,25 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
-function updateProgress() {
-    console.log()
+function handleProgress() {
+    const percent = (video.currentTime/video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
 }
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+  }
 
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', scrub);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip))
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
-
-// progressBar.addEventListener('play', updateProgress)
-
-console.log(progressBar)
