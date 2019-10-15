0e4b558b2c0296611eb1cd78ef8e9a58c8ae4a61
cdrishinski <cdrishinski@gmail.com>
2019-10-14 18:40:03 -0600

    finishing progress bar functionality

--- scripts.js
+++ scripts.js
@@ -44,10 +44,17 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
-progress.addEventListener('click', scrub);
+
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip))
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener
+('mousemove', handleRangeUpdate))
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true)
+progress.addEventListener('mouseup', () => mousedown = false)
